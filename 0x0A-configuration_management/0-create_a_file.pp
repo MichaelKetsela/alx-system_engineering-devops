92f0908c110c00fadca20cda3adc7c7f77370cf9
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2023-03-24 12:15:43 -0930

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file in /tmp
 
 file {'/tmp/school':
-    path     => 'file',
+    path     => 'present',
     mode     => '0744',
     owner    => 'www-data',
     group    => 'www-data',
