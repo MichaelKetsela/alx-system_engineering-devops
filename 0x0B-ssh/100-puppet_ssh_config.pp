9f558ae2c84f20cbd7d83e149527b4dc4532bb84
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2023-03-26 12:32:03 -0930

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #Puppet file to create ssh config file
 file_line{'turn off password':
     ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
+    path   => '/bin/',
     line   => 'PasswordAuthentication no',
 }
 
 file_line{ 'Declare idebtity file'
     ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
+    path   => '/bin/',
     line   => 'IdentityFile ~/.ssh/school',
 }
